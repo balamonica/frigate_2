b10b40ebd2cf063b2209bebf0bc9b66bc7eddf2e
Marc Altmann <40744649+MarcA711@users.noreply.github.com>
2025-01-03 18:09:27 +0100

    rockchip: update dependencies and add script for model conversion (#15699)
    * rockchip: update dependencies and add script for model conversion
    
    * rockchip: update docs
    
    ---------
    
    Co-authored-by: Nicolas Mowen <nickmowen213@gmail.com>

--- frigate/detectors/plugins/rknn.py
+++ frigate/detectors/plugins/rknn.py
@@ -158,7 +158,7 @@ class Rknn(DetectionApi):
                     model_props["model_type"] = model_type
 
             if model_matched:
-                model_props["filename"] = model_path + f"-{soc}-v2.0.0-1.rknn"
+                model_props["filename"] = model_path + f"-{soc}-v2.3.0-1.rknn"
 
                 model_props["path"] = model_cache_dir + model_props["filename"]
 
@@ -179,7 +179,7 @@ class Rknn(DetectionApi):
             os.mkdir(model_cache_dir)
 
         urllib.request.urlretrieve(
-            f"https://github.com/MarcA711/rknn-models/releases/download/v2.0.0/{filename}",
+            f"https://github.com/MarcA711/rknn-models/releases/download/v2.3.0/{filename}",
             model_cache_dir + filename,
         )
 
